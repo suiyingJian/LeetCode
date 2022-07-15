8401a9d5984949e252a79bd76e838a992368f732
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-15 17:10:21 +0800

    7_15_2.0

---
+++ 链表/002_环形链表二.js
@@ -0,0 +1,11 @@
+var deteCycle = function (head) {
+    while (head) {
+        if (head.flag) {
+            return head
+        } else {
+            head.flag = true
+            head = head.next
+        }
+    }
+    return null;
+}
