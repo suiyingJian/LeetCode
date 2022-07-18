a31b3387008691a7109815445e0730534afc14bb
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-18 18:01:04 +0800

    7/18_02

---
+++ 206.反转链表.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let p1 = head 
+    let p2 = null
+    while (p1) {
+        // console.log(p1.val,p2 && p2.val);
+
+        const tmp = p1.next
+        p1.next = p2
+        p2 = p1
+        p1 = tmp
+
+    }
+    // console.log(p1 && p1.val, p2 && p2.val);
+    return p2
+};
---
+++ 链表/json.js
@@ -0,0 +1,13 @@
+const json = {
+    a: { b: { c: 1 } },
+    d: { e: 2 }
+}
+
+// const path = ['a', 'b', 'c']
+const path = ['a', 'b']
+
+
+let p = json
+path.forEach(k => {
+    p = p[k]
+})
