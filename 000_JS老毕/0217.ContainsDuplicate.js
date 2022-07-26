9b44b7dcbaa3710a753121b6ded5ea6005254d5a
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-27 07:22:58 +0800

    7/27_01

---
+++ 000_JS老毕/0217.ContainsDuplicate.js
@@ -0,0 +1,20 @@
+/**
+ * 存在重复元素
+ * @param {number[]} nums
+ * @return {boolean}
+ * 
+ * set   .has
+ */
+var containsDuplicate = function(nums) {
+  const set = new Set()
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
+    }
+
+    set.add(nums[i]);
+  }
+
+  return false;
+};
---
+++ 000_JS老毕/0219.ContainsDuplicateII.js
@@ -0,0 +1,19 @@
+/**
+ * 存在重复元素II
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]) && (i - map.get(nums[i] )<= k)) {
+      return true;
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+
+  return false
+};
