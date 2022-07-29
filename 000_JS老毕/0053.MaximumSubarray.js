62d06b5db666481deb020c400033e57144006472
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-29 10:31:05 +0800

    LC_53

--- 000_JS老毕/0053.MaximumSubarray.js
+++ 000_JS老毕/0053.MaximumSubarray.js
@@ -1,7 +1,24 @@
 /**
+ * 
+ * 找出最大子段和 
+ * 动态规划思想
+ * 比较当前数组元素和之前数组元素之和，取大的那个抉择是新开一个数组还是添加进去
  * @param {number[]} nums
  * @return {number}
  */
 var maxSubArray = function(nums) {
+  const memo = [];
+  memo[0] = nums[0]
+  let max = nums[0]
 
+  for (let i = 1; i < nums.length; i++) {
+    memo[i] = Math.max(nums[i] + memo[i - 1], nums[i])
+    max = Math.max(max,memo[i])
+  }
+
+  // for (let i = 1; i < memo.length; i++){
+  //   max = Math.max(max,memo[i])
+  // }
+
+  return max;
 };
