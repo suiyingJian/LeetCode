07c96b99cade09dbe07367462f72c9c12e8eda21
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-30 08:22:20 +0800

    LC_19

--- 000_JS老毕/0019.js
+++ 000_JS老毕/0019.js
@@ -1,5 +1,5 @@
+
 /**
- * 19. 删除链表的倒数第 N 个结点
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
@@ -18,20 +18,15 @@ var removeNthFromEnd = function(head, n) {
     let n1 = dummy;
     let n2 = dummy;
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i <= n; i++) {
         n2 = n2.next;
     }
-
-    while (n2.next != null) {
+    while (n2 != null) {
         n1 = n1.next;
         n2 = n2.next;
+        
     }
+
     n1.next = n1.next.next
-
-    return dummy.next;
+    return dummy.next
 };
-
-
-/**
- * 双指针
- */
