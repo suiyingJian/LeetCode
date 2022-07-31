72a792aae2ac6e1931a3a136fca81917dcfe545a
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-31 17:18:13 +0800

    7-31_七月最后一天—暑假过半—力扣题数混到七十题

--- 000_JS老毕/jslaobi-leetcode-js-master/0053.MaximumSubarray.js
+++ 000_JS老毕/jslaobi-leetcode-js-master/0053.MaximumSubarray.js
@@ -1,3 +1,12 @@
+/**
+ * 
+ * 找出最大子段和 
+ * 动态规划思想
+ * 比较当前数组元素和之前数组元素之和，取大的那个抉择是新开一个数组还是添加进去
+ * @param {number[]} nums
+ * @return {number}
+ */
+
 var maxSubArray = function (nums) {
   const memo = [];
   memo[0] = nums[0];
